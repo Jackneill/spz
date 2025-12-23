fa038a1bbb2252140a6b77d4d6a2376d1e3264dd
Jackneill <jackneill1000@gmail.com>
2025-12-23 16:48:55 +0100

    feat: add skeleton pyspz

---
+++ crates/pyspz/src/lib.rs
@@ -0,0 +1,11 @@
+use pyo3::prelude::*;
+
+#[pymodule]
+mod spz {
+	use pyo3::prelude::*;
+
+	#[pyfunction]
+	fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+		Ok((a + b).to_string())
+	}
+}
